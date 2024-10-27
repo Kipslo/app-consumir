9734dec4ca7761cbce1e1edd170fe103c1761f38
Kipslo <gbrlrdrgsdslvl@gmail.com>
2024-10-27 18:21:59 -0300

    Create app.py

---
+++ app.py
@@ -0,0 +1,51 @@
+import flet as ft
+import socket
+class app():
+    def __init__(self):
+        ft.app(target=self.main)
+    def sendstr(self, data):
+        self.PORT = 55261
+        self.HOST = "26.108.30.186"
+        self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.s.connect((self.HOST, self.PORT))
+        self.s.sendall(str.encode(data))
+        data = self.s.recv(1024)
+        self.s.shutdown(socket.SHUT_RDWR)
+        self.s.close()
+        return data.decode()
+    
+    def main(self, page: ft.Page):
+        def initpage():
+            passflet
+        def login(event):
+            data = ""
+            data = self.sendstr("LOGIN," + self.entry_name.value + "," + self.entry_password.value)
+            data = data
+            print(data)
+            if data == "YES":
+                self.name, self.password = self.entry_name.value, self.entry_password.value
+                page.clean()
+            elif data == "NOT":
+                self.errorlogintext.value = "NOME E/OU SENHA INCORRETOS"
+                self.errorlogintext.update()
+        
+        
+        page.horizontal_alignment = "center"
+        page.vertical_alignment = "center"
+
+        page.title = "APP"
+
+        self.entry_name = ft.TextField(label="NOME", width=350)
+
+        self.entry_password = ft.TextField(label="SENHA", width=350)
+
+        self.errorlogintext = ft.Text(value="", width=350, height=50, size=22)
+
+        conteiner = ft.Container(content=ft.Column([ft.Container(width=500, height=100), self.entry_name, self.entry_password, ft.ElevatedButton("LOGIN", width=350, height=50, on_click=login), self.errorlogintext], horizontal_alignment= "center"), width=500, height=500, bgcolor=ft.colors.BLACK12)
+        
+
+
+        loginarea = ft.Column([conteiner], horizontal_alignment="center")
+
+        page.add(loginarea)
+app()
