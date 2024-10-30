0c57dbc59593d8132c9e647f4097e6c2545189f7
Kipslo <gbrlrdrgsdslvl@gmail.com>
2024-10-30 15:27:57 -0300

    Update app.py

--- app.py
+++ app.py
@@ -17,24 +17,34 @@ class app():
     def main(self, page: ft.Page):
         def initpage(event = 0):
             def commandpage(number):
-                pass
-            page.scroll = "always"
+                page.clean()
+                page.scroll = "always"
+                page.appbar = ft.AppBar(bgcolor="#efefef", title=ft.Text("COMANDA " + str(number)) ,actions=[ft.ElevatedButton(text="Voltar", on_click=initpage)])
+                
+                products = self.sendstr("PRODUCTS," + str(number))
+                print(products)
+                products = products.split(",")
+                print(products)
+                for k, i in enumerate(products):
+                    products[k] = i.split("|")
+                print(products)
+                for i in products:
+                    page.add(ft.Row(ft.Container(content=(), height=49, alignment="center"), height=50))
+            page.appbar = ft.AppBar(bgcolor="#efefef", actions=[ft.ElevatedButton(text="Recarregar", on_click=initpage)])
             page.clean()
+            page.scroll = "always"
             limitcommands = (self.sendstr("LIMITCOMMANDS"))
-            print(limitcommands)
             opencommands = self.sendstr("OPENCOMMANDS")
             opencommands = opencommands.split(",")
-            print(opencommands)
 
-
-            head = ft.Row([ft.TextButton(text="Recarregar", on_click=initpage, width=150, height=75)], width=150, alignment="top-center")
             commands = ft.Row(wrap=True)
             for i in range(int(limitcommands)):
-                if str(i + 1) in opencommands:
-                    commands.controls.append(ft.ElevatedButton(text=str(i + 1), on_click=lambda y, x = i + 1: commandpage(x), color=ft.colors.BLACK, bgcolor=ft.colors.RED, width=100,height=50))
+                n = i + 1
+                if str(n) in opencommands:
+                    commands.controls.append(ft.ElevatedButton(text=str(n), on_click=lambda y, x = i + 1: commandpage(x), color=ft.colors.BLACK, bgcolor=ft.colors.RED_700, width=100,height=50))
                 else:
-                    commands.controls.append(ft.ElevatedButton(text=str(i + 1), on_click=lambda y, x = i + 1: commandpage(x), color=ft.colors.BLACK, bgcolor=ft.colors.GREEN, width=100,height=50))
-            main = ft.Column([head, commands], horizontal_alignment="center")
+                    commands.controls.append(ft.ElevatedButton(text=str(n), on_click=lambda y, x = i + 1: commandpage(x), color=ft.colors.BLACK, bgcolor=ft.colors.GREEN, width=100,height=50))
+            main = ft.Column([commands], horizontal_alignment="center")
             page.add(main)
 
         def login(event):
